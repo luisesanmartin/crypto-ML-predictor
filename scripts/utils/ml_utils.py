7c8d09c4920d1667e73cd6e27e745a8afabc5c82
luisesanmartin <luise.sanmartin@gmail.com>
2021-04-02 17:57:52 -0400

    added xgb train function

---
+++ scripts/utils/ml_utils.py
@@ -0,0 +1,13 @@
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def train_xgboost(X_train, Y_train):
+
+    '''
+    '''
+
+    model = XGBClassifier()
+    model.fit(X_train, Y_train)
+
+    return model
